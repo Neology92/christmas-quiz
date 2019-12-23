dd38c510f7334e6a54bd3b7688e2ef696ae66db0
Neology92 <oskarlegner@gmail.com>
2019-12-23 23:15:04 +0100

    Set mechanics and style

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const form = document.querySelector("#form1");
-const inputs = form.querySelectorAll(
-  "input[required], textarea[required], select[required]"
-);
+let haslo = [];
+let form = [];
+let quiz = [];
 
-form.setAttribute("novalidate", true);
+haslo[0] = "asdf";
+haslo[1] = "asdg";
+haslo[2] = "asdv";
+haslo[3] = "asdb";
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault(); //przerywamy domyślną wysyłkę
+form[0] = document.querySelector("#form1");
+form[1] = document.querySelector("#form2");
+form[2] = document.querySelector("#form3");
+form[3] = document.querySelector("#form4");
 
-  //dalsza część kodu wraz z ewentualną wysyłką
-  if (wszystko_ok) {
-    this.submit();
-  }
-});
+quiz[0] = document.querySelector("#q1");
+quiz[1] = document.querySelector("#q2");
+quiz[2] = document.querySelector("#q3");
+quiz[3] = document.querySelector("#q4");
+
+for (let i = 0; i < 4; i += 1) {
+  form[i].setAttribute("novalidate", true);
+  const input = form[i].querySelector('input[name="response"]');
+
+  form[i].addEventListener("keyup", function(e) {
+    console.log(input.value);
+    if (input.value === haslo[i]) {
+      quiz[i].classList.add("done");
+    }
+
+    //dalsza część kodu wraz z ewentualną wysyłką
+    //   if (wszystko_ok) {
+    //   this.submit();
+    //   }
+  });
+
+  form[i].addEventListener("submit", function(e) {
+    e.preventDefault(); //przerywamy domyślną wysyłkę
+  });
+}
